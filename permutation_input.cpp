8bf4077ac2c7b5591050344b9ce58752082c0835
asish <asishm.school@gmail.com>
2019-10-30 23:33:15 -0400

    made intial input work now need to make the permutations of this

---
+++ permutation_input.cpp
@@ -0,0 +1,14 @@
+//this script takes in a input (single word) and outputs all the possible
+//combinations (or permutations) of the word
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main(){
+
+	std::string bob;
+	std::cin >> bob;
+	std::cout << bob;
+
+
+}
