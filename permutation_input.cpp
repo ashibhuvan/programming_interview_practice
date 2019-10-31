3204dbb19e4421bb46905aa92c6b9e6bacecdccb
asish <asishm.school@gmail.com>
2019-10-31 00:27:10 -0400

    checked it works and gives the correct answer

--- permutation_input.cpp
+++ permutation_input.cpp
@@ -4,11 +4,35 @@
 #include <string>
 #include <iostream>
 
+
+void permutation_helper(std::string& S,  std::string& character){
+	if(S == ""){
+	std::cout << character << std::endl;
+	}
+
+	for(auto& it : S){
+		int index = &it - &S[0];
+		char  c = it;
+		character = character + c;
+		S.erase(index,1);
+		permutation_helper(S,character);
+		S.insert(index,1,c);		
+		character.erase(character.size()-1,1);
+	}
+
+} 
+void permutation(std::string S){
+	
+	std::string character = "";
+	permutation_helper(S, character);
+}
+
 int main(){
 
-	std::string bob;
-	std::cin >> bob;
-	std::cout << bob;
-
-
+	std::string input;
+	std::cin >> input;
+	std::cout << "The input is : " << input << std::endl;
+	
+	permutation(input);
+	return 0;
 }
