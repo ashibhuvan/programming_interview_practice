05feb7ade3c853764e6663d482b2d046ae22b206
asish <asishm.school@gmail.com>
2019-11-07 22:16:51 -0500

    ?

--- rotateImage.cpp
+++ rotateImage.cpp
@@ -18,12 +18,23 @@ rotateImage(a) =
      [8, 5, 2],
      [9, 6, 3]]
 */
+void rotateImage(std::vector<std::vector<int>> a){
 
+	std::vector<std::vector<int>> b(a);
+	b[0][0] = 10;	
+	for(auto& it : b){
+		for(auto& it2: it){std::cout << it2 << ",";  }
+		std::cout << "\n" ;
+	}
+
+
+}
 int main(){
 	std::vector<std::vector<int>> a = { {1,2,3},{4,5,6},{7,8,9}};
 	for(auto& it : a){
 		for(auto& it2: it){std::cout << it2 << ",";  }
 		std::cout << "\n" ;
 	}
+	rotateImage(a);
 	return 0;	
 }
